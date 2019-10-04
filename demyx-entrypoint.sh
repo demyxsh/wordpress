6504fe77b3d3cd69dfd76e605c2b5e215486a385
Demyx <info@demyx.sh>
2019-10-04 03:46:14 -0700

    Lower case flag

--- demyx-entrypoint.sh
+++ demyx-entrypoint.sh
@@ -4,7 +4,7 @@
 
 if [[ ! -d /var/www/html/wp-admin ]]; then
 	echo "WordPress is missing, installing now."
-	cp -R /usr/src/wordpress/* /var/www/html
+	cp -r /usr/src/wordpress/* /var/www/html
 
 	if [[ "$WORDPRESS_DB_NAME" ]] && [[ "$WORDPRESS_DB_USER" ]] && [[ "$WORDPRESS_DB_PASSWORD" ]] && [[ "$WORDPRESS_DB_HOST" ]]; then
 		mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
