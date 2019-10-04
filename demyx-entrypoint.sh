10cdd17d1c746e65a18fea7eaac554c486bb838f
Demyx <info@demyx.sh>
2019-10-04 03:23:50 -0700

    Make curl silent

--- demyx-entrypoint.sh
+++ demyx-entrypoint.sh
@@ -12,7 +12,7 @@ if [[ ! -d /var/www/html/wp-admin ]]; then
 		sed -i "s/username_here/$WORDPRESS_DB_USER/g" /var/www/html/wp-config.php
 		sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wp-config.php
 		sed -i "s/localhost/$WORDPRESS_DB_HOST/g" /var/www/html/wp-config.php 
-		SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
+		SALT=$(curl -sL https://api.wordpress.org/secret-key/1.1/salt/)
 		printf '%s\n' "g/put your unique phrase here/d" a "$SALT" . w | ed -s /var/www/html/wp-config.php
 		sed -i "s/$table_prefix = 'wp_';/$table_prefix = 'wp_';\n\n\/\/ If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact\n\/\/ see also http:\/\/codex.wordpress.org\/Administration_Over_SSL#Using_a_Reverse_Proxy\nif (isset($\_SERVER['HTTP_X_FORWARDED_PROTO']) \&\& $\_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {\n\t$\_SERVER['HTTPS'] = 'on';\n}\n/g" /var/www/html/wp-config.php
 	fi
