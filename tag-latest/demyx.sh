d5273c1f5d5d24a409b8db1dd52d4c52bf10c86f
Demyx <info@demyx.sh>
2019-11-12 01:18:59 -0800

    Add environment variables for PHP's process manager

--- tag-latest/demyx.sh
+++ tag-latest/demyx.sh
@@ -54,6 +54,41 @@ if [[ "$WORDPRESS_PHP_OPCACHE" = off || "$WORDPRESS_PHP_OPCACHE" = false ]]; the
     sed -i "s|opcache.enable_cli=1|opcache.enable_cli=0|g" /demyx/php.ini
 fi
 
+# PHP process manager
+if [[ -n "$WORDPRESS_PHP_PM" ]]; then
+    sed -i "s|pm = ondemand|pm = $WORDPRESS_PHP_PM|g" /demyx/www.conf
+fi
+
+# PHP process manager max children
+if [[ -n "$WORDPRESS_PHP_PM_MAX_CHILDREN" ]]; then
+    sed -i "s|pm.max_children = 100|pm.max_children = $WORDPRESS_PHP_PM_MAX_CHILDREN|g" /demyx/www.conf
+fi
+
+# PHP process manager start servers
+if [[ -n "$WORDPRESS_PHP_PM_START_SERVERS" ]]; then
+    sed -i "s|pm.start_servers = 2|pm.start_servers = $WORDPRESS_PHP_PM_START_SERVERS|g" /demyx/www.conf
+fi
+
+# PHP process manager start servers
+if [[ -n "$WORDPRESS_PHP_PM_MIN_SPARE_SERVERS" ]]; then
+    sed -i "s|pm.min_spare_servers = 1|pm.min_spare_servers = $WORDPRESS_PHP_PM_MIN_SPARE_SERVERS|g" /demyx/www.conf
+fi
+
+# PHP process manager start servers
+if [[ -n "$WORDPRESS_PHP_PM_MAX_SPARE_SERVERS" ]]; then
+    sed -i "s|pm.max_spare_servers = 3|pm.max_spare_servers = $WORDPRESS_PHP_PM_MAX_SPARE_SERVERS|g" /demyx/www.conf
+fi
+
+# PHP process manager start servers
+if [[ -n "$WORDPRESS_PHP_PM_PROCESS_IDLE_TIMEOUT" ]]; then
+    sed -i "s|pm.process_idle_timeout = 10s|pm.process_idle_timeout = $WORDPRESS_PHP_PM_PROCESS_IDLE_TIMEOUT|g" /demyx/www.conf
+fi
+
+# PHP process manager start servers
+if [[ -n "$WORDPRESS_PHP_PM_MAX_REQUESTS" ]]; then
+    sed -i "s|pm.max_requests = 500|pm.max_requests = $WORDPRESS_PHP_PM_MAX_REQUESTS|g" /demyx/www.conf
+fi
+
 # Timezone
 if [[ "$TZ" ]]; then
     sed -i "s|UTC|$TZ|g" /demyx/php.ini
