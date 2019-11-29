621887d442d9ca0a06454ebd1fcdab7bda894aca
Demyx <info@demyx.sh>
2019-11-29 01:06:11 -0800

    Update install script

--- tag-bedrock/install.sh
+++ tag-bedrock/install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # Demyx
 # https://demyx.sh
+set -euo pipefail
+
+# Define variables
+WORDPRESS_DOMAIN="${WORDPRESS_DOMAIN:-domain.tld}"
+WORDPRESS_SSL="${WORDPRESS_SSL:-false}"
+WORDPRESS_DB_NAME="${WORDPRESS_DB_NAME:-}"
+WORDPRESS_DB_USER="${WORDPRESS_DB_USER:-}"
+WORDPRESS_DB_PASSWORD="${WORDPRESS_DB_PASSWORD:-}"
+WORDPRESS_DB_HOST="${WORDPRESS_DB_HOST:-}"
 
 if [[ ! -f /var/www/html/.env ]]; then
-    echo "[demyx] installing WordPress..."
+    echo "[demyx] installing Bedrock..."
     tar -xzf /usr/src/bedrock.tgz -C /var/www/html
 
     if [[ -n "$WORDPRESS_DB_NAME" && -n "$WORDPRESS_DB_USER" && -n "$WORDPRESS_DB_PASSWORD" && -n "$WORDPRESS_DB_HOST" && -n "$WORDPRESS_DOMAIN" ]]; then
@@ -16,6 +25,7 @@ if [[ ! -f /var/www/html/.env ]]; then
         sed -i "s|# DB_HOST=localhost|DB_HOST=$WORDPRESS_DB_HOST|g" /var/www/html/.env
         SALT="$(curl -sL https://api.wordpress.org/secret-key/1.1/salt/ | sed "s|define('||g" | sed "s|',|=|g" | sed "s| ||g" | sed "s|);||g")"
         printf '%s\n' "g/generateme/d" a "$SALT" . w | ed -s /var/www/html/.env
+        sed -i "s|WP_ENV=.*|WP_ENV=production|g" /var/www/html/.env
     else
         echo "One or more environment variables are missing!"
         exit 1
