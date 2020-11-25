dc9fa232ca293b7e2aa271b8a908a59d22c32091
Demyx <info@demyx.sh>
2020-11-25 00:19:25 -0800

    Travis-CI Updates - Use full paths - Update variables - Update shell script

--- travis.sh
+++ travis.sh
@@ -1,58 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 # Demyx
 # https://demyx.sh
-# https://github.com/peter-evans/dockerhub-description/blob/master/entrypoint.sh
-IFS=$'\n\t'
 
 # Get versions
-DEMYX_WORDPRESS_ALPINE_VERSION="$(docker exec "$DEMYX_REPOSITORY" cat /etc/os-release | grep VERSION_ID | cut -c 12- | sed -e 's/\r//g')"
-DEMYX_WORDPRESS_PHP_VERSION="$(docker exec "$DEMYX_REPOSITORY" php -v | grep cli | awk -F '[ ]' '{print $2}' | sed -e 's/\r//g')"
-DEMYX_WORDPRESS_VERSION="$(docker run --rm --volumes-from="$DEMYX_REPOSITORY" --network container:"$DEMYX_REPOSITORY" demyx/"$DEMYX_REPOSITORY":cli core version | sed -e 's/\r//g')"
-DEMYX_WORDPRESS_BEDROCK_VERSION="$(curl -sL https://api.github.com/repos/roots/bedrock/releases/latest | grep '"tag_name"' | head -n1 | awk -F '[:]' '{print $2}' | sed -e 's/"//g' | sed -e 's/,//g' | sed -e 's/ //g' | sed -e 's/\r//g')"
-DEMYX_WORDPRESS_CLI_VERSION="$(docker run --rm demyx/"$DEMYX_REPOSITORY":cli --version | awk -F '[ ]' '{print $2}' | sed -e 's/\r//g')"
+DEMYX_WORDPRESS_ALPINE_VERSION="$(/usr/bin/docker exec "$DEMYX_REPOSITORY" /bin/cat /etc/os-release | /bin/grep VERSION_ID | /usr/bin/cut -c 12- | /bin/sed 's/\r//g')"
+DEMYX_WORDPRESS_PHP_VERSION="$(/usr/bin/docker exec "$DEMYX_REPOSITORY" /usr/local/bin/php -v | /bin/grep cli | /usr/bin/awk -F '[ ]' '{print $2}' | /bin/sed 's/\r//g')"
+DEMYX_WORDPRESS_VERSION="$(/usr/bin/docker run --rm --volumes-from="$DEMYX_REPOSITORY" --network container:"$DEMYX_REPOSITORY" demyx/"$DEMYX_REPOSITORY":cli core version | /bin/sed 's/\r//g')"
+DEMYX_WORDPRESS_BEDROCK_VERSION="$(curl -sL https://api.github.com/repos/roots/bedrock/releases/latest | /bin/grep '"tag_name"' | /usr/bin/head -n1 | /usr/bin/awk -F '[:]' '{print $2}' | /bin/sed 's/"//g' | /bin/sed 's/,//g' | /bin/sed 's/ //g' | /bin/sed 's/\r//g')"
+DEMYX_WORDPRESS_CLI_VERSION="$(/usr/bin/docker run --rm demyx/"$DEMYX_REPOSITORY":cli --version | /usr/bin/awk -F '[ ]' '{print $2}' | /bin/sed 's/\r//g')"
 
 # Replace versions
-sed -i "s|alpine-.*.-informational|alpine-${DEMYX_WORDPRESS_ALPINE_VERSION}-informational|g" README.md
-sed -i "s|php-.*.-informational|php-${DEMYX_WORDPRESS_PHP_VERSION}-informational|g" README.md
-sed -i "s|wordpress-.*.-informational|wordpress-${DEMYX_WORDPRESS_VERSION}-informational|g" README.md
-sed -i "s|bedrock-.*.-informational|bedrock-${DEMYX_WORDPRESS_BEDROCK_VERSION}-informational|g" README.md
-sed -i "s|wp--cli-.*.-informational|wp--cli-${DEMYX_WORDPRESS_CLI_VERSION}-informational|g" README.md
+/bin/sed -i "s|alpine-.*.-informational|alpine-${DEMYX_WORDPRESS_ALPINE_VERSION}-informational|g" README.md
+/bin/sed -i "s|php-.*.-informational|php-${DEMYX_WORDPRESS_PHP_VERSION}-informational|g" README.md
+/bin/sed -i "s|wordpress-.*.-informational|wordpress-${DEMYX_WORDPRESS_VERSION}-informational|g" README.md
+/bin/sed -i "s|bedrock-.*.-informational|bedrock-${DEMYX_WORDPRESS_BEDROCK_VERSION}-informational|g" README.md
+/bin/sed -i "s|wp--cli-.*.-informational|wp--cli-${DEMYX_WORDPRESS_CLI_VERSION}-informational|g" README.md
 
 # Echo version to file
-echo "DEMYX_WORDPRESS_ALPINE_VERSION=$DEMYX_WORDPRESS_ALPINE_VERSION
+/bin/echo "DEMYX_WORDPRESS_ALPINE_VERSION=$DEMYX_WORDPRESS_ALPINE_VERSION
 DEMYX_WORDPRESS_PHP_VERSION=$DEMYX_WORDPRESS_PHP_VERSION
 DEMYX_WORDPRESS_VERSION=$DEMYX_WORDPRESS_VERSION
 DEMYX_WORDPRESS_BEDROCK_VERSION=$DEMYX_WORDPRESS_BEDROCK_VERSION
 DEMYX_WORDPRESS_CLI_VERSION=$DEMYX_WORDPRESS_CLI_VERSION" > VERSION
 
 # Push back to GitHub
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git remote set-url origin https://${DEMYX_GITHUB_TOKEN}@github.com/demyxco/"$DEMYX_REPOSITORY".git
+/usr/bin/git config --global user.email "travis@travis-ci.com"
+/usr/bin/git config --global user.name "Travis CI"
+/usr/bin/git remote set-url origin https://"$DEMYX_GITHUB_TOKEN"@github.com/demyxco/"$DEMYX_REPOSITORY".git
 # Commit VERSION first
-git add VERSION
-git commit -m "DEMYX_WORDPRESS_ALPINE_VERSION $DEMYX_WORDPRESS_ALPINE_VERSION, DEMYX_WORDPRESS_PHP_VERSION $DEMYX_WORDPRESS_PHP_VERSION, DEMYX_WORDPRESS_VERSION $DEMYX_WORDPRESS_VERSION, DEMYX_WORDPRESS_BEDROCK_VERSION $DEMYX_WORDPRESS_BEDROCK_VERSION, DEMYX_WORDPRESS_CLI_VERSION $DEMYX_WORDPRESS_CLI_VERSION"
-git push origin HEAD:master
+/usr/bin/git add VERSION
+/usr/bin/git commit -m "DEMYX_WORDPRESS_ALPINE_VERSION $DEMYX_WORDPRESS_ALPINE_VERSION, DEMYX_WORDPRESS_PHP_VERSION $DEMYX_WORDPRESS_PHP_VERSION, DEMYX_WORDPRESS_VERSION $DEMYX_WORDPRESS_VERSION, DEMYX_WORDPRESS_BEDROCK_VERSION $DEMYX_WORDPRESS_BEDROCK_VERSION, DEMYX_WORDPRESS_CLI_VERSION $DEMYX_WORDPRESS_CLI_VERSION"
+/usr/bin/git push origin HEAD:master
 # Commit the rest
-git add .
-git commit -m "Travis Build $TRAVIS_BUILD_NUMBER"
-git push origin HEAD:master
-
-# Set the default path to README.md
-README_FILEPATH="./README.md"
-
-# Acquire a token for the Docker Hub API
-echo "Acquiring token"
-TOKEN="$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'$DEMYX_USERNAME'", "password": "'$DEMYX_PASSWORD'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)"
+/usr/bin/git add .
+/usr/bin/git commit -m "Travis Build $TRAVIS_BUILD_NUMBER"
+/usr/bin/git push origin HEAD:master
 
 # Send a PATCH request to update the description of the repository
-echo "Sending PATCH request"
-REPO_URL="https://hub.docker.com/v2/repositories/${DEMYX_USERNAME}/${DEMYX_REPOSITORY}/"
-RESPONSE_CODE=$(curl -s --write-out %{response_code} --output /dev/null -H "Authorization: JWT ${TOKEN}" -X PATCH --data-urlencode full_description@${README_FILEPATH} ${REPO_URL})
-echo "Received response code: $RESPONSE_CODE"
+/bin/echo "Sending PATCH request"
+DEMYX_DOCKER_TOKEN="$(/usr/bin/curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"$DEMYX_USERNAME"'", "password": "'"$DEMYX_PASSWORD"'"}' "https://hub.docker.com/v2/users/login/" | /usr/local/bin/jq -r .token)"
+DEMYX_RESPONSE_CODE="$(/usr/bin/curl -s --write-out "%{response_code}" --output /dev/null -H "Authorization: JWT ${DEMYX_DOCKER_TOKEN}" -X PATCH --data-urlencode full_description@"README.md" "https://hub.docker.com/v2/repositories/${DEMYX_USERNAME}/${DEMYX_REPOSITORY}/")"
+/bin/echo "Received response code: $DEMYX_RESPONSE_CODE"
 
-if [ $RESPONSE_CODE -eq 200 ]; then
-  exit 0
-else
-  exit 1
-fi
+# Return an exit 1 code if response isn't 200
+[[ "$DEMYX_RESPONSE_CODE" != 200 ]] && exit 1
